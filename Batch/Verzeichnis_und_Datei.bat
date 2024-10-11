99cd7ca32373fbbddc65a067ffaa9ef5a0228d5e
Andreas <Andreas.Pesch@innolog.de>
2024-10-11 15:28:26 +0200

    Aufgeräumt und eigene Befehls-Dateien hinzugefügt.

--- Batch/Verzeichnis_und_Datei.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Verzeichnis 1: %cd%
-echo Batch-Datei  : %0
-echo Verzeichnis 2: %~dp0
-
-set path=%~dp0
-for %%i in (%path:~0,-1%) do set parent=_%%~nxi
-echo Parent: %parent%
-pause
